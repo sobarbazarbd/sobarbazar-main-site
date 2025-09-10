61363f41b4cf47ce1d79abbe083e869e75c10970
unknown <sobarbazarbd1@gmail.com>
2025-09-10 11:09:20 +0600

    category images added

--- src/components/BottomFooter.jsx
+++ src/components/BottomFooter.jsx
@@ -6,7 +6,7 @@ const BottomFooter = () => {
       <div className='container container-lg'>
         <div className='bottom-footer__inner flex-between flex-wrap gap-16 py-16'>
           <p className='bottom-footer__text '>
-            Marketpro eCommerce © 2024. All Rights Reserved{" "}
+            SobarBazarBd © 2025. All Rights Reserved{" "}
           </p>
           <div className='flex-align gap-8 flex-wrap'>
             <span className='text-heading text-sm'>We Are Acepting</span>
